48f4be0de41ededf0461c27c39b0ef11ec1e5d8e
Juan-Garcia1 <juan_garcia_1995@hotmail.com>
2020-03-31 11:52:14 -0500

    create contact page

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,92 @@
+@extends('layouts.app')
+
+@section('content')
+<section class="page-header faq_page" style="background-image:url(http://themes.webmasterdriver.net/carforyouwp/wp-content/uploads/2017/01/contact-page-header-img.jpg )">
+
+    <div class="container">
+  
+      <div class="page-header_wrap">
+  
+        <div class="page-heading">
+  
+          <h1>Contact Us</h1>
+  
+        </div>
+  
+      </div>
+  
+    </div>
+  
+    <!-- Dark Overlay-->
+  
+    <div class="dark-overlay"></div>
+  
+  </section>
+  
+  <!-- /Page Header--> 
+      <div class="container">
+          <div class="row">
+            <article id="post-81" class="post-81 page type-page status-publish hentry">
+                  <div class="vc_row wpb_row vc_row-fluid vc_custom_1487329145162"><div class="contact_us section-padding wpb_column vc_column_container vc_col-sm-12"><div class="vc_column-inner"><div class="wpb_wrapper"><div class="vc_row wpb_row vc_inner vc_row-fluid"><div class="wpb_column vc_column_container col-sm-6"><div class="vc_column-inner"><div class="wpb_wrapper"><h3 style="text-align: left" class="vc_custom_heading" >Get in touch using the form below</h3>
+      <div class="wpb_text_column wpb_content_element  vc_custom_1486128244194" >
+          <div class="wpb_wrapper">
+              <div role="form" class="wpcf7" id="wpcf7-f143-p81-o1" lang="en-US" dir="ltr">
+  <div class="screen-reader-response"></div>
+  <form action="/carforyouwp/contact-us/#wpcf7-f143-p81-o1" method="post" class="wpcf7-form" novalidate="novalidate">
+  <div style="display: none;">
+  <input type="hidden" name="_wpcf7" value="143" />
+  <input type="hidden" name="_wpcf7_version" value="5.1.6" />
+  <input type="hidden" name="_wpcf7_locale" value="en_US" />
+  <input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f143-p81-o1" />
+  <input type="hidden" name="_wpcf7_container_post" value="81" />
+  </div>
+  <div class="contact_form gray-bg">
+  <div class="form-group">
+                              <label for="fullname" class="control-label">Full Name <span>*</span></label><br />
+                               <span class="wpcf7-form-control-wrap text"><input type="text" name="text" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required form-control white_bg" id="fullname" aria-required="true" aria-invalid="false" /></span>
+                          </div>
+  <div class="form-group">
+                              <label for="emailaddress" class="control-label">Email Address <span>*</span></label><br />
+  <span class="wpcf7-form-control-wrap email"><input type="email" name="email" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email form-control white_bg" id="emailaddress" aria-required="true" aria-invalid="false" /></span>
+                          </div>
+  <div class="form-group">
+                              <label for="phonenumber" class="control-label">Phone Number <span>*</span></label><br />
+   <span class="wpcf7-form-control-wrap number"><input type="number" name="number" value="" class="wpcf7-form-control wpcf7-number wpcf7-validates-as-required wpcf7-validates-as-number form-control white_bg" id="number" aria-required="true" aria-invalid="false" /></span>
+                          </div>
+  <div class="form-group">
+                              <label for="message" class="control-label">Message <span>*</span></label><br />
+                              <span class="wpcf7-form-control-wrap message"><textarea name="message" cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea wpcf7-validates-as-required form-control white_bg" aria-required="true" aria-invalid="false"></textarea></span>
+                          </div>
+  <div class="form-group">
+                              <input type="submit" value="Send Message" class="wpcf7-form-control wpcf7-submit btn" />
+                          </div></div>
+  <div class="wpcf7-response-output wpcf7-display-none"></div></form></div>
+  
+          </div>
+      </div>
+  </div></div></div><div class="wpb_column vc_column_container col-sm-6"><div class="vc_column-inner"><div class="wpb_wrapper"><h3 style="text-align: left" class="vc_custom_heading" >Contact Info</h3>
+      <div class="wpb_raw_code wpb_content_element wpb_raw_html" >
+          <div class="wpb_wrapper">
+              <div class="contact_detail">
+            <ul>
+              <li>
+                <div class="icon_wrap"><i class="fa fa-map-marker" aria-hidden="true"></i></div>
+                <div class="contact_info_m">PO Box 1025MNO Collins Street West Victoria 8007 Australia</div>
+              </li>
+              <li>
+                <div class="icon_wrap"><i class="fa fa-phone" aria-hidden="true"></i></div>
+                <div class="contact_info_m"><a href="tel:61-1234-567-90">+61-123-456-789</a></div>
+              </li>
+              <li>
+                <div class="icon_wrap"><i class="fa fa-envelope-o" aria-hidden="true"></i></div>
+                <div class="contact_info_m"><a href="mailto:contact@exampleurl.com">contact@exampleurl.com</a></div>
+              </li>
+            </ul>
+          </div>
+          </div>
+      </div>
+  <div class="wpb_gmaps_widget wpb_content_element vc_custom_1486128033105 map_wrap"><div class="wpb_wrapper"><div class="wpb_map_wraper"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6304.829986131271!2d-122.4746968033092!3d37.80374752160443!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808586e6302615a1%3A0x86bd130251757c00!2sStorey+Ave%2C+San+Francisco%2C+CA+94129!5e0!3m2!1sen!2sus!4v1435826432051" width="558" height="440" frameborder="0" style="border:0" allowfullscreen></iframe></div></div></div></div></div></div></div></div></div></div></div>
+             </article>
+          </div>
+      </div>
+@endsection
